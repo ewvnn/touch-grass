46ca40f10c7c452958ba0524426e0910324a5a41
ewvnn <ewenchai0@gmail.com>
2025-10-19 15:58:20 +0800

    delete self.vue

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,12 @@ import {
 } from "vue-router";
 import Index from '/src/pages/Index.vue'
 import Calendar from '/src/pages/Calendar.vue'
-import Itinerary from '/src/pages/Itinerary.vue';
+import Itinerary from '/src/pages/Itinerary.vue'
 
 const routes = [
   { path: '/', component: Index },         // Landing page
   { path: '/calendar', component: Calendar }, // Calendar page
-  { path: '/itinerary', component: Itinerary} // itinerary page
-  { path: '/self', component: Self} // Own Profile
+  { path: '/itinerary', component: Itinerary}, // itinerary page
 ]
 
 const router = createRouter({
