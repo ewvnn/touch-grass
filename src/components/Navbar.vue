3d64286abf8500e377e26500b973084b84bf79a5
xt.lai.2024@smu.edu.sg <xt.lai.2024@smu.edu.sg>
2025-10-19 15:10:39 +0800

    edit navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,12 +37,12 @@
 
         <!-- Right: Icons -->
         <ul class="navbar-nav ms-auto align-items-lg-center gap-lg-2">
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link d-flex align-items-center gap-2" href="/profile" aria-label="Profile" title="Profile">
             <i class="bi bi-person-circle fs-5"></i>
             <span class="d-inline d-lg-none d-xl-inline">Profile</span>
           </a>
-        </li>
+        </li> -->
         <li class="nav-item">
           <a class="nav-link d-flex align-items-center gap-2" href="/calendar" aria-label="Favourites" title="Favourites">
             <i class="bi bi-calendar fs-5"></i>
@@ -53,6 +53,12 @@
           <a class="nav-link d-flex align-items-center gap-2" href="/itinerary" aria-label="Itinerary planner" title="Itinerary planner">
             <i class="bi bi-signpost-2 fs-5"></i>
             <span class="d-inline d-lg-none d-xl-inline">Itinerary</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link d-flex align-items-center gap-2" href="/login" aria-label="Login" title="Login">
+            <i class="bi bi-person-circle fs-5"></i>
+            <span class="d-inline d-lg-none d-xl-inline">Login</span>
           </a>
         </li>
       </ul>
